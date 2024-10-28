52b67f4bee9aaf2e29dc2406a95507e436a728d6
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-10-28 13:41:53 +0900

    update

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 
 // just return query string
-echo $_SERVER['QUERY_STRING'];
+// echo $_SERVER['REQUEST_URI'];
+// echo $_SERVER['QUERY_STRING'];
+
+// return as json
+$v = array(
+    'REQUEST_URI' => $_SERVER['REQUEST_URI'],
+    'QUERY_STRING' => $_SERVER['QUERY_STRING']
+);
+
+echo json_encode($v);
